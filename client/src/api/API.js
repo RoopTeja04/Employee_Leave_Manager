70660531263d218a98095b8dbb986479bc678644
Roop Teja <roopteja112@gmail.com>
2025-08-31 20:29:57 +0530

    frontend api updated

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "http://localhost:5000",
+    baseURL: "https://employee-leave-manager-1.onrender.com",
     headers: { "Content-Type": "application/json" }
 
 })
